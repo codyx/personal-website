28b7e951389395c4718a9aeeeab1a4923745e06c
codyx <aimeric.seguin@epitech.eu>
2017-04-25 03:34:52 -0400

    Set up development environment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// @flow
+
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <h3>Welcome to my App</h3>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+// @flow
+
+const webpack = require('webpack');
+const path = require('path');
+
+const APP_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, 'public/dist');
+const PORT = process.env.APP_PORT || 3000;
+
+const config = {
+  entry: [
+    'react-hot-loader/patch', // Activate HMR for React
+    `webpack-dev-server/client?http://localhost:${PORT}`,
+    'webpack/hot/only-dev-server',
+    `${APP_DIR}/index.js`,
+  ],
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: '/statics/' // Necessary for HMR to know where to load the hot update chunks
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'inline-source-map',
+  module: {
+      loaders: [
+        {
+          test: /.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        }
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(), // Enable HMR globally
+      new webpack.NamedModulesPlugin(), // Prints more readable module names in the browser console on HMR updates
+      new webpack.NoEmitOnErrorsPlugin(), // Do not emit compiled assets that include errors
+  ],
+  devServer: {
+    host: 'localhost',
+    port: PORT,
+    historyApiFallback: true, // Respond to 404s with index.html
+    hot: true,
+  },
+};
+
+module.exports = config;
