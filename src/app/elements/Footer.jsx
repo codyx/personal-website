161986d61a9e732b45b9f083b43ac6651b96d556
codyx <aimeric.seguin@epitech.eu>
2017-05-08 11:33:28 -0400

    Add footer

--- src/app/elements/Footer.jsx
+++ src/app/elements/Footer.jsx
@@ -1,26 +1,48 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 import Layout from 'material-ui/Layout';
-import Chip from 'material-ui/Chip';
+import Button from 'material-ui/Button';
+import Avatar from 'material-ui/Avatar';
 import styled from 'styled-components';
 
 const CustomPaper = styled(Paper)`
+    margin: auto;
+    text-align: center;
     padding: 16px;
-    text-align: 'center';
     overflow: hidden;
 `;
 
 CustomPaper.muiName = 'Paper';
 
-const Title = styled.h3`
-  text-align: center;
+const CustomAvatar = styled(Avatar)`
+  width: 64px;
+  height: 64px;
 `;
+
+CustomAvatar.muiName = 'Avatar';
+
+const Credits = styled.div`
+  position: relative;
+  margin-top: 10px;
+  font-size: 8px;
+`
 
 const Footer = () => (
   <Layout item xs={12}>
     <CustomPaper>
-      <Title>Connect with me</Title>
-      <Chip label="Twitter" />
+      <Button fab primary>
+        <a href="mailto:aimeric.seguin@gmail.com"><CustomAvatar src="dist/png/email.png" alt="email"></CustomAvatar></a>
+      </Button>
+      <Button fab primary>
+        <a href="https://twitter.com/AimericSeguin" target="_blank" rel="noopener noreferrer"><CustomAvatar src="dist/png/twitter.png" alt="twitter" /></a>
+      </Button>
+      <Button fab primary>
+        <a href="https://www.linkedin.com/in/aimeric-seguin-084a14b2/" target="_blank" rel="noopener noreferrer">
+          <CustomAvatar src="dist/png/linkedin.png" alt="linkedin" />
+        </a>
+      </Button>
+      <Credits>Icons made by <a href="http://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></Credits>
+      <Credits>Aimeric Seguin &copy; {new Date().getFullYear()}</Credits>
     </CustomPaper>
   </Layout>
 );
