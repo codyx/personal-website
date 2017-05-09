752d854e7bf34991d0534dad9678c63016865b8d
codyx <aimeric.seguin@epitech.eu>
2017-05-09 18:56:22 -0400

    Remove boot loader

--- src/index.js
+++ src/index.js
@@ -5,7 +5,3 @@ import ReactDOM from 'react-dom';
 import App from './app/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// const LOADER_BOOT_STATIC_DURATION_IN_MS = 500;
-// setTimeout(() => {
-//  }, LOADER_BOOT_STATIC_DURATION_IN_MS);
