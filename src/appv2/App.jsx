ca6729ba4713627e37bfe69bcbc4457d64a40202
Aimeric Seguin <aimeric.seguin@gmail.com>
2018-10-26 16:34:38 +0200

    Add styled-components babel plugin

--- src/appv2/App.jsx
+++ src/appv2/App.jsx
@@ -1,16 +1,33 @@
 // @flow
 
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { default as MUIButton } from '@material-ui/core/Button';
+import styled, { css } from 'styled-components';
 
 type Props = {};
+
+const Button = styled(MUIButton)`
+	background: transparent;
+	border-radius: 3px;
+	border: 2px solid palevioletred;
+	color: palevioletred;
+	margin: 0 1em;
+	padding: 0.25em 1em;
+
+	${props =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `};
+`;
 
 export default class App extends React.Component<Props> {
     render() {
         return (
-					<Button variant="contained" color="primary">
-            Hi!
-          </Button>
+					<>
+						<Button variant="contained" color="primary">Hey</Button>
+					</>
         );
     }
 }
